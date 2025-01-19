0dafaa8ed824f72a8a57c4c47ced97f8db039b77
Nada-09 <nadaalmajidi4@gmail.com>
2025-01-19 09:39:18 +0300

    Initial Commit

---
+++ BakeryHome/BakeryHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BakeryHomeApp.swift
+//  BakeryHome
+//
+//  Created by Nada Abdullah on 19/07/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct BakeryHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BakeryHomeTests/BakeryHomeTests.swift
@@ -0,0 +1,17 @@
+//
+//  BakeryHomeTests.swift
+//  BakeryHomeTests
+//
+//  Created by Nada Abdullah on 19/07/1446 AH.
+//
+
+import Testing
+@testable import BakeryHome
+
+struct BakeryHomeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
